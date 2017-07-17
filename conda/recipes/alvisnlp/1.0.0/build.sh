ef141bb970e632d148f32bc17df5ad4ff4f042ad
bamouhamadou <mandiayba@gmail.com>
2017-07-17 13:55:30 +0200

    conda receipt for alvisnlp

---
+++ conda/recipes/alvisnlp/1.0.0/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+outdir=$PREFIX/share/$PKG_NAME
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+
+mvn clean install
+./install.sh $outdir
+
+ls $outdir
+chmod +x $outdir/bin/alvisnlp
+ln -s $outdir/bin/alvisnlp $PREFIX/bin/alvisnlp
