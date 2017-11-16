63fd1639b57087e343dd5945754dcb98dae87206
bamouhamadou <mandiayba@gmail.com>
2017-11-16 08:19:55 +0100

    adds

--- openminted-components/alvis-br-train/train.sh
+++ openminted-components/alvis-br-train/train.sh
@@ -14,9 +14,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-alvisnlp -param $1 textDir $2 \
-         -param $3 textDir $4 \
-         -param $5 textDir $6 \
-         -param TEESTrain $7 $8 \
-	  /alvisnlp/plans/train.plan
+COMMAND="alvisnlp"
+if [ "$1" == "alvisnlp" ]; then
+    COMMAND = $1
+else
+    echo "Bad command"
+fi
+
+# parameter not used here
+INPUT="input"
+if [ "$2" == "input" ]; then
+    INPUT = $2
+else
+    echo "Bad command input"
+fi
+
+# parameter not used here
+INPUT="output" 
+if [ "$3" == "output" ]; then
+    OUTPUT = $3
+else
+    echo "Bad command output"
+fi
+
+TRAIN = $4
+TRAIN_PATH_VALUE = $5
+
+DEV = $6
+DEV_PATH_VALUE = $7
+
+TEST = $8
+TEST_PATH_VALUE = $9
+
+MODEL = $10
+MODEL_PATH_VALUE = $11
+
+
+$COMMAND  -param $TRAIN extDir $TRAIN_PATH_VALUE \
+    	-param $DEV textDir $DEV_PATH_VALUE \
+    	-param $TEST textDir $TEST_PATH_VALUE \
+    	-param TEESTrain $MODEL $MODEL_PATH_VALUE \
+    	/alvisnlp/plans/train.plan
 
