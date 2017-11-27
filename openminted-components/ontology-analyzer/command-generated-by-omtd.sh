d1370b5604c084d4cc4b50798374ae7db6caafe6
mouhamadou ba <mandiayba@gmail.com>
2017-11-27 16:46:31 +0100

    Update command-generated-by-omtd.sh

--- openminted-components/ontology-analyzer/command-generated-by-omtd.sh
+++ openminted-components/ontology-analyzer/command-generated-by-omtd.sh
@@ -1,1 +1,5 @@
-docker run -i --rm -v $PWD/test-data:/alvisnlp/data -a stderr bibliome/alvis-br-train alvisnlp org.bibliome.alvisnlp.modules.tees.alvis-br-train --input /alvisnlp/data/corpus --output /alvisnlp/data/models --param:train=/alvisnlp/data/corpus/train --param:dev=/alvisnlp/data/corpus/dev --param:test=/alvisnlp/data/corpus/test --param:model=/alvisnlp/data/models
+docker run -i --rm -v $PWD/test-data:/alvisnlp/data -a stderr \
+bibliome/ontology-analyzer \
+alvisnlp org.bibliome.alvisnlp.modules.phenotype-ontology-analyzer \
+--input /alvisnlp/data/ontology/OntoBiotope-Phenotype-v2.obo \
+--output /alvisnlp/data/result/OntoBiotope-Phenotype.tomap
